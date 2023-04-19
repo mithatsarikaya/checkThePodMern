d1a17a6a16d12750a057155fc75858a66493e46d
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-04-19 20:15:32 +0300

    adding more css, decided font family

--- checkThePodReactJs/src/App.jsx
+++ checkThePodReactJs/src/App.jsx
@@ -12,37 +12,55 @@ export default function App() {
         <div className="pod">
           <div className="podName">
             <label htmlFor="">Pod Name</label>
-            <h2 className="podName--title">Pod Tavuk</h2>
+            <h3 className="podName--title">Pod Tavuk</h3>
           </div>
           <div className="podTare">
             <label htmlFor="">Pod Tare</label>
-            <h2 className="podName--tare">200</h2>
+            <h3 className="podName--tare">200</h3>
           </div>
           <div className="podTotal">
             <label htmlFor="">Pod Total</label>
-            <h2 className="podName--total">1000</h2>
+            <h3 className="podName--total">1000</h3>
           </div>
           <div className="podRaw">
             <label htmlFor="">Raw Product</label>
-            <h2 className="podName--raw">300</h2>
+            <h3 className="podName--raw">300</h3>
           </div>
         </div>
         <div className="pod">
           <div className="podName">
             <label htmlFor="">Pod Name</label>
-            <h2 className="podName--title">Pod Pilav</h2>
+            <h3 className="podName--title">Pod Pilav</h3>
           </div>
           <div className="podTare">
             <label htmlFor="">Pod Tare</label>
-            <h2 className="podName--tare">300</h2>
+            <h3 className="podName--tare">300</h3>
           </div>
           <div className="podTotal">
             <label htmlFor="">Pod Total</label>
-            <h2 className="podName--total">1200</h2>
+            <h3 className="podName--total">1200</h3>
           </div>
           <div className="podRaw">
             <label htmlFor="">Raw Product</label>
-            <h2 className="podName--raw">400</h2>
+            <h3 className="podName--raw">400</h3>
+          </div>
+        </div>
+        <div className="pod">
+          <div className="podName">
+            <label htmlFor="">Pod Name</label>
+            <h3 className="podName--title">Pod Pilav</h3>
+          </div>
+          <div className="podTare">
+            <label htmlFor="">Pod Tare</label>
+            <h3 className="podName--tare">300</h3>
+          </div>
+          <div className="podTotal">
+            <label htmlFor="">Pod Total</label>
+            <h3 className="podName--total">1200</h3>
+          </div>
+          <div className="podRaw">
+            <label htmlFor="">Raw Product</label>
+            <h3 className="podName--raw">400</h3>
           </div>
         </div>
       </main>

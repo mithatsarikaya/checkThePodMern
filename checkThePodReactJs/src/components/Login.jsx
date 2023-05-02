1f131a5d11df21a20d40aa3c9b27f407d2a62d87
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-05-02 21:21:14 +0300

    trying to authenticate the user

--- checkThePodReactJs/src/components/Login.jsx
+++ checkThePodReactJs/src/components/Login.jsx
@@ -1,17 +1,62 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
   const navigate = useNavigate();
+  const url = "http://localhost:3500/";
+
+  const [loginInfos, setLoginInfos] = useState({ username: "", password: "" });
+
+  const handleChange = (e) => {
+    setLoginInfos((prevInfos) => ({
+      ...prevInfos,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    let response = await fetch(`${url}users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginInfos),
+    });
+    console.log(response);
+  };
 
   const sendUserToRegisterPage = () => {
     navigate("/register");
   };
+
   return (
     <main>
-      <form action="">
-        <input type="email" name="" id="" required placeholder="email" />
-        <input type="password" name="" id="" required placeholder="password" />
-        <input className="form--button" type="submit" value="LOGIN" />
+      <form action="" onSubmit={handleLogin}>
+        <input
+          tabIndex={1}
+          onChange={handleChange}
+          type="text"
+          name="username"
+          id=""
+          required
+          placeholder="username"
+        />
+        <input
+          tabIndex={2}
+          onChange={handleChange}
+          type="password"
+          name="password"
+          id=""
+          required
+          placeholder="password"
+        />
+        <input
+          tabIndex={3}
+          className="form--button"
+          type="submit"
+          value="LOGIN"
+        />
         <input
           className="form--button"
           onClick={sendUserToRegisterPage}

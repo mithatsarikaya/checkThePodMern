d0953dfdb4d7e9c5bb67d1d191f7d2ce8b89f4ea
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-05-04 23:50:41 +0300

    added: login logout

--- checkThePodReactJs/src/components/Nav.jsx
+++ checkThePodReactJs/src/components/Nav.jsx
@@ -4,18 +4,25 @@ import useAuth from "../hooks/useAuth";
 export default function Nav() {
   const { auth, setAuth } = useAuth();
 
+  const username = auth?.username;
   return (
     <>
       <header>Check The Pods</header>
-      {auth?.username && <h1>Welcome {auth.username}</h1>}
+      {username && <h1>Welcome {username}</h1>}
       <nav>
         <ul>
           <Link tabIndex={1} to="/">
             <li>HOME</li>
           </Link>
-          <Link tabIndex={2} to="/login">
-            <li>LOGIN</li>
-          </Link>
+          {username ? (
+            <Link tabIndex={2} to="/login">
+              <li>LOGOUT</li>
+            </Link>
+          ) : (
+            <Link tabIndex={2} to="/login">
+              <li>LOGIN</li>
+            </Link>
+          )}
           <Link tabIndex={3} to="/createPod">
             <li>Create Pod</li>
           </Link>

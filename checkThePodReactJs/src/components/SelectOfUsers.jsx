17e2949fc731dc32b831fdbb11609d66edbce617
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-05-30 22:20:41 +0300

    fixed: when user select user with mobile phone, the first element of the selectbox was picked default, now user can decide after seeing the options

--- checkThePodReactJs/src/components/SelectOfUsers.jsx
+++ checkThePodReactJs/src/components/SelectOfUsers.jsx
@@ -2,7 +2,7 @@ import OptionUser from "./OptionUser";
 
 export default function SelectOfUsers({ users, addToPod }) {
   return (
-    <select onClick={(e) => addToPod(e.target.value)} name="" id="">
+    <select onChange={(e) => addToPod(e.target.value)} name="" id="">
       {users.map((user) => (
         <OptionUser user={user} />
       ))}
--- checkThePodReactJs/src/hooks/useFetch.jsx
+++ checkThePodReactJs/src/hooks/useFetch.jsx
@@ -2,7 +2,8 @@ import useAuth from "./useAuth";
 
 export default function useFetch() {
   const { auth } = useAuth();
-  const BASE_URL = "http://localhost:3500/";
+  // const BASE_URL = "http://localhost:3500/";
+  const BASE_URL = "http://192.168.43.47:3500/";
   const TOKEN = auth.token;
 
   const fetchFromUser = async (method, url, body) => {

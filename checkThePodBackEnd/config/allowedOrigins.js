3240374b5b438240f4908b5688826756e60d731b
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-05-31 20:27:13 +0300

    fe url updated

--- checkThePodBackEnd/config/allowedOrigins.js
+++ checkThePodBackEnd/config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
   // "http://localhost:5173",
   // "https://www.baslanbas.com",
   // "http://192.168.43.47:5173",
-  "https://check-the-pod-mern.vercel.app/",
+  "https://check-the-pod-mern.vercel.app",
 ];
 
 module.exports = allowedOrigins;

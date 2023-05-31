8874babe228fcf15736740369aade2d0809ac16a
mithatsarikaya <mithatsarikaya58@gmail.com>
2023-05-31 21:27:49 +0300

    fe url updated5

--- checkThePodReactJs/src/hooks/useFetch.jsx
+++ checkThePodReactJs/src/hooks/useFetch.jsx
@@ -8,7 +8,7 @@ export default function useFetch() {
   const TOKEN = auth.token;
 
   const fetchFromUser = async (method, url, body) => {
-    let responseFromServer = await fetch(`${BASE_URL}/${url}`, {
+    let responseFromServer = await fetch(`${BASE_URL}${url}`, {
       method: `${method}`,
       headers: {
         "Content-Type": "application/json",
@@ -25,7 +25,7 @@ export default function useFetch() {
 
   const fetchPublic = async (method, url, body) => {
     console.log(`${BASE_URL}${url}`);
-    let responseFromServer = await fetch(`${BASE_URL}/${url}`, {
+    let responseFromServer = await fetch(`${BASE_URL}${url}`, {
       method: `${method}`,
       headers: {
         "Content-Type": "application/json",
